2f6c9986530a0d69fc030d64a0ee3ed6724790a0
Ori Gerbi <origerbi19@gmail.com>
2021-03-27 23:14:30 +0300

    First Test to check Jenkins

--- main.py
+++
@@ -1,9 +0,0 @@
-
-def print_hi(name):
-    print("Hello Ori's Server")
-
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-
---
+++ pythonProject/main.py
@@ -0,0 +1,9 @@
+
+
+def print_hi(name):
+    print(f'Hi, {name}', 'This is your test')
+
+
+if __name__ == '__main__':
+    print_hi('Ori')
+
